53b2274408d157c6f86255dec33bc5cb75b2f3d8
Mason Jennings <mjennings@pinterest.com>
2020-03-07 18:12:43 -0800

    Show all bookings on the bookings page

--- frontend/src/Pages/Bookings.js
+++ frontend/src/Pages/Bookings.js
@@ -1,8 +1,64 @@
 import React, { Component } from 'react';
+import AuthContext from '../context/auth-context';
 
 class BookingsPage extends Component {
+  static contextType = AuthContext;
+
+  state = { bookings: [], isLoading: false };
+
+  componentDidMount() {
+    this.fetchBookings();
+  }
+
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+
+    const requestBody = {
+      query: `
+        query {
+          bookings {
+            _id
+            createdAt
+            event {
+              _id
+              title
+              date
+            }
+          }
+        }
+      `,
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.context.token,
+      },
+    }).then(res => {
+      if (![200, 201].includes(res.status)) {
+        throw new Error('Failed!')
+      }
+
+      return res.json();
+    })
+    .then(resData => {
+      this.setState({ isLoading: false });
+      const bookings = resData.data.bookings;
+      this.setState({ bookings });
+    }).catch(err => {
+      this.setState({ isLoading: false });
+      console.log(err);
+    });
+  };
+
   render() {
-    return (<h1>The Bookings Page</h1>)
+    const { bookings } = this.state;
+
+    return (<ul>
+      {bookings.map((booking) => (<li key={booking._id}>{booking.event.title} - {new Date(booking.createdAt).toLocaleDateString()}</li>))}
+    </ul>)
   }  
 }
 
