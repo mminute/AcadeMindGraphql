4810d0877f1425ce3b649bd179d42eca65e92cab
Mason Jennings <mjennings@pinterest.com>
2020-03-15 22:30:23 -0700

    Try adding the authorization header to Apollo Client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import AuthContext from './context/auth-context';
 import AuthPage from './Pages/Auth';
 import BookingsPage from './Pages/Bookings';
 import EventsPage from './Pages/Events';
 import MainNavigation from './Components/Navigation';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -26,8 +29,28 @@ class App extends Component {
   render() {
     const { token, userId } = this.state;
 
+    const httpLink = createHttpLink({
+      uri: 'http://localhost:8000/graphql',
+    });
+
+    const authLink = setContext((_, { headers }) => {
+      // return the headers to the context so httpLink can read them
+      return {
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : "",
+        }
+      }
+    });
+
+    const client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache(),
+    });
+
+
     return (
-      <ApolloProvider client={new ApolloClient({ uri: 'http://localhost:8000/graphql' })}>
+      <ApolloProvider client={client}>
         <BrowserRouter>
           <Fragment>
             <AuthContext.Provider
